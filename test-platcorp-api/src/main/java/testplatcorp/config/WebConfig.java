1619b627c387d4946ac19b78b3dbba5529af9e71
AlePezzutto <ale.pezzutto@gmail.com>
2019-08-14 21:55:45 -0300

    Adicionando suporte a CORS

---
+++ test-platcorp-api/src/main/java/testplatcorp/config/WebConfig.java
@@ -0,0 +1,19 @@
+package testplatcorp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("*/**")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", 
+						        "PATCH", "CONNECT", "TRACE", "HEAD");
+	}
+	
+	
+	
+	
+}
